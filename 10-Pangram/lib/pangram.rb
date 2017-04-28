735669068d80eda1528cd50a5cb385d9f57cc87d
Jason Cheung <jckaichu@gmail.com>
2017-04-28 17:27:00 +0100

    done

--- 10-Pangram/lib/pangram.rb
+++ 10-Pangram/lib/pangram.rb
@@ -1,4 +1,6 @@
 def pangram?(sentence)
   # TODO: return true/false depending if the sentence is a pangram (contains every letter of the alphabet)
-
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  sentence = sentence.downcase.gsub(/\W/, "").chars.sort.join.squeeze
+  alphabet == sentence
 end
