c08f2d94e6a4bc44dd669e7b1ca119625f2908e9
Jason Cheung <jckaichu@gmail.com>
2017-04-28 17:05:24 +0100

    done

--- 08-Palindrome-sentence/lib/better_palindrome.rb
+++ 08-Palindrome-sentence/lib/better_palindrome.rb
@@ -1,4 +1,5 @@
 def better_palindrome?(sentence)
   # TODO: return true/false depending if the *sentence* is a palindrome
-
+  sentence = sentence.downcase.gsub(/\W/, "")
+  sentence == sentence.reverse ? (return true) : (return false)
 end
