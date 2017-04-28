6b7f992195c542bb05ee0e1f3adcdbde317466c2
Jason Cheung <jckaichu@gmail.com>
2017-04-28 16:47:03 +0100

    done

--- 05-Pythagorean-triples/lib/pythagorean_triple.rb
+++ 05-Pythagorean-triples/lib/pythagorean_triple.rb
@@ -1,4 +1,8 @@
 def pythagorean_triple?(a, b, c)
   # TODO: return true/false depending if numbers a, b and c are Pythagorean triples
-
+  if a.is_a?(Fixnum) && b.is_a?(Fixnum) && c.is_a?(Fixnum)
+    a ** 2 + b ** 2 == c ** 2 ? (return true) : (return false)
+  else
+    return false
+  end
 end
