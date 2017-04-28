3b84eddb90d8cbf6c9d990789bfef721d2c2ebf4
Jason Cheung <jckaichu@gmail.com>
2017-04-28 17:01:46 +0100

    done

--- 07-Word-frequency/lib/word_frequency.rb
+++ 07-Word-frequency/lib/word_frequency.rb
@@ -1,4 +1,11 @@
 def word_frequency(sentence)
   # TODO: return a hash representing the number of apperances of words in the sentence
+  words = sentence.downcase.gsub(/[!@%&,?]/, "").split(" ")
 
+  word_hash = Hash.new { 0 }
+
+  words.each do |e|
+    word_hash[e] += 1
+  end
+  return word_hash
 end
