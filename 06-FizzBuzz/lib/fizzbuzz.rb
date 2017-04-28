732c8076e66577fb17b61b78ed4626ee2bf826fa
Jason Cheung <jckaichu@gmail.com>
2017-04-28 16:54:38 +0100

    done

--- 06-FizzBuzz/lib/fizzbuzz.rb
+++ 06-FizzBuzz/lib/fizzbuzz.rb
@@ -1,4 +1,16 @@
 def fizzbuzz(max)
   # TODO: return an Array
+  arr = (1..max).to_a
 
+  arr.each_with_index do |e, i|
+    if (e % 3).zero? && (e % 5).zero?
+      arr[i] = "FizzBuzz"
+    elsif (e % 5).zero?
+      arr[i] = "Buzz"
+    elsif (e % 3).zero?
+      arr[i] = "Fizz"
+    end
+  end
+
+  return arr
 end
