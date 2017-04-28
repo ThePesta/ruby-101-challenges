8f097ad11ac120f4806dc00f1258acac20bef410
Jason Cheung <jckaichu@gmail.com>
2017-04-28 17:08:52 +0100

    done

--- 09-DNA-transcription/lib/transcription.rb
+++ 09-DNA-transcription/lib/transcription.rb
@@ -1,4 +1,17 @@
 def dna_transcription(dna_sequence)
   # TODO: return the RNA complement of the incoming DNA sequence
-
+  rna = []
+  dna_sequence.each do |e|
+    case e.upcase
+    when "G"
+      rna << "C"
+    when "C"
+      rna << "G"
+    when "T"
+      rna << "A"
+    when "A"
+      rna << "U"
+    end
+  end
+  return rna
 end
