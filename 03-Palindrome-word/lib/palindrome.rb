7a0ee5c80ec87b27dc31113192ea1eb9337de5e5
Jason Cheung <jckaichu@gmail.com>
2017-04-28 16:39:23 +0100

    done

--- 03-Palindrome-word/lib/palindrome.rb
+++ 03-Palindrome-word/lib/palindrome.rb
@@ -1,4 +1,5 @@
 def palindrome?(word)
   # TODO: return true/false depending if the *word* is a palindrome
-
+  word = word.downcase.gsub(" ", "")
+  word == word.reverse ? (return true) : (return false)
 end
