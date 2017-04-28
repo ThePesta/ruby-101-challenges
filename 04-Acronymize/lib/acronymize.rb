4e135e29ff8eae4769996c7002919e07913132e2
Jason Cheung <jckaichu@gmail.com>
2017-04-28 16:41:38 +0100

    done

--- 04-Acronymize/lib/acronymize.rb
+++ 04-Acronymize/lib/acronymize.rb
@@ -1,4 +1,7 @@
 def acronymize(sentence)
   # TODO: return the acronym on the given sentence
-
+  words = sentence.split(" ")
+  acronym = ""
+  words.each {|word| acronym << word[0].upcase}
+  return acronym
 end
